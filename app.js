c1eb709b85bad393fef16cf43a201eaa16401ed1
MuhammadMoazzamKhan <124440588+MuhammadMoazzamKhan@users.noreply.github.com>
2023-04-27 02:11:09 +0500

    Object, Ternary operator and Switch

---
+++ app.js
@@ -0,0 +1,74 @@
+// let a = {
+//     culture:"the way of life, especially the general customs and beliefs, of a particular group of people at a particular time",
+//     yakka: "work",
+//     vow:"to make a determined decision or promise to do something",
+//     beggar:"a person, especially when you are expressing an opinion about something that they have done, or that has happened to them",
+//     rubbish:"waste material or things that are no longer wanted or needed"
+// }
+
+// console.log(a["culture"]);
+// console.log(a["yakka"]);
+// console.log(a["vow"]);
+// console.log(a["beggar"]);
+// console.log(a["rubbish"]);
+
+
+// let a = prompt("Enter your age for driving");
+// let age = parseInt(a);
+// if(age > 50){
+//     console.log("You can't drive");
+// }
+// else if(age < 18){
+//     console.log("You can't drive");
+// }
+// else if (age <= 18 && age <= 50){
+//     console.log("You can drive");
+// }else{
+//     console.log("nikal")
+// }
+
+// console.log("You could", a<18? "not drive":"drive") //Ternory operator .
+
+// const cups = 45;
+// switch (cups){
+//     case 45:
+//         console.log("The value is cup of 4")
+//         break;
+//     default:
+//         console.log("nikal nikal nikal")
+
+// }
+
+/*
+let age = prompt("Enter your age");
+switch (age) {
+    case "10":
+        console.log("Your age is special 10")
+        break;
+    case "11":
+        console.log("Your age is special 11")
+        break;
+    case "12":
+        console.log("Your age is special 12")
+        break;
+    case "13":
+        console.log("Your age is special 13")
+        break;
+    case "14":
+        console.log("Your age is special 14")
+        break;
+    case "15":
+        console.log("Your age is special 15")
+        break;
+    case "16":
+        console.log("Your age is special 16")
+        break;
+    default:
+        console.log("Your age is not special "+ age);
+} 
+*/
+
+/*
+let age = 19;
+console.log("you can", age<18? "not drive":"drive");//This is called ternory operator..
+*/
